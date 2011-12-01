7fa8171c261cba34bc6c6267742717f172dd320f
bernard <bernard@093daca5-0a66-41c3-9b22-06d954382fe3>
2011-12-01 17:51:13 +0000

    New EntityConverter implementation.
    git-svn-id: http://it-crowd.com.pl/svn/seam3-persistence-framework/trunk@8 093daca5-0a66-41c3-9b22-06d954382fe3

--- src/main/java/pl/com/it_crowd/seam/framework/converter/EntityConverter.java
+++ src/main/java/pl/com/it_crowd/seam/framework/converter/EntityConverter.java
@@ -1,35 +1,63 @@
 package pl.com.it_crowd.seam.framework.converter;
 
+import pl.com.it_crowd.seam.framework.Identifiable;
+
 import javax.enterprise.context.RequestScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 @RequestScoped
-@FacesConverter("entityConverter")
+@FacesConverter(value = "entityConverter")
 public class EntityConverter implements Converter {
 // ------------------------------ FIELDS ------------------------------
 
+    private static final String NULL_ENTITY = "null";
+
+    private static final String TRANSIENT_ENTITY = "new";
+
     @Inject
-    private EntityIdentifierStore entityIdentifierStore;
+    private EntityManager entityManager;
 
 // ------------------------ INTERFACE METHODS ------------------------
 
 
 // --------------------- Interface Converter ---------------------
 
-    public Object getAsObject(final FacesContext facesContext, final UIComponent component, final String id)
+    public Object getAsObject(FacesContext context, UIComponent component, String value)
     {
-        return entityIdentifierStore.get(id);
+        Class entityClass = getEntityClass(context, component);
+        if (NULL_ENTITY.equals(value)) {
+            return null;
+        } else if (TRANSIENT_ENTITY.equals(value)) {
+            try {
+                return entityClass.newInstance();
+            } catch (Exception e) {
+                throw new ConverterException("Cannot instantiate new object of type " + entityClass.getCanonicalName());
+            }
+        } else {
+            return entityManager.find(entityClass, Long.parseLong(value));
+        }
     }
 
-    public String getAsString(final FacesContext context, final UIComponent comp, final Object entity)
+    public String getAsString(FacesContext context, UIComponent component, Object value)
     {
-        if (entity != null) {
-            return entityIdentifierStore.put(entity);
+        if (value == null) {
+            return NULL_ENTITY;
         }
-        return null;
+        if (!(value instanceof Identifiable)) {
+            throw new ConverterException(String.format("Class %s doesn't implemente Indentifiable<Long> interface", value.getClass()));
+        }
+        Object id = ((Identifiable) value).getId();
+        return id == null ? TRANSIENT_ENTITY : id.toString();
+    }
+
+    protected Class getEntityClass(FacesContext context, UIComponent component)
+    {
+        return component.getValueExpression("value").getType(context.getELContext());
     }
 }
